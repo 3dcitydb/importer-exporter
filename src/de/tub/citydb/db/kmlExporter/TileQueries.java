58a1cb31a3f65381ed8c2d5743870d5080ad92b4
Javier Herreruela <javier.herreruela@tu-berlin.de>
2011-03-07 10:29:02 +0000

    Added missing queries for LoD4.

--- src/de/tub/citydb/db/kmlExporter/TileQueries.java
+++ src/de/tub/citydb/db/kmlExporter/TileQueries.java
@@ -8,6 +8,89 @@ import de.tub.citydb.log.Logger;
 
 public class TileQueries {
 
+	private static final String QUERY_FOOTPRINT_LOD4_GET_BUILDING_DATA_ALT =
+		"SELECT SDO_CS.TRANSFORM(SDO_AGGR_UNION(SDOAGGRTYPE(sg.geometry, 0.05)), 4326), " +
+				"MAX(b.id) AS building_id " +
+		"FROM CITYOBJECT co, BUILDING b, SURFACE_GEOMETRY sg " +
+		"WHERE " +
+			"co.gmlid = ? " +
+		"AND b.id = co.id " +
+		"AND sg.root_id = b.lod4_geometry_id " +
+		"AND sg.geometry IS NOT NULL " +
+		"GROUP BY b.id " +
+		"ORDER BY b.id";
+
+	private static final String QUERY_FOOTPRINT_LOD4_GET_BUILDING_DATA =
+		"SELECT SDO_CS.TRANSFORM(sg.geometry, 4326) " +
+		"FROM SURFACE_GEOMETRY sg, THEMATIC_SURFACE ts, CITYOBJECT co " +
+		"WHERE " +
+			"co.gmlid = ? " +
+			"AND ts.building_id = co.id " +
+			"AND ts.type = 'GroundSurface' " +
+			"AND sg.root_id = ts.lod4_multi_surface_id " +
+			"AND sg.geometry IS NOT NULL " +
+		"ORDER BY ts.building_id";
+
+	private static final String QUERY_EXTRUDED_LOD4_GET_BUILDING_DATA_ALT =
+		"SELECT SDO_CS.TRANSFORM(SDO_AGGR_UNION(SDOAGGRTYPE(sg.geometry, 0.05)), 4326), " +
+				"MAX(b.id) AS building_id, MAX(SDO_GEOM.SDO_MAX_MBR_ORDINATE(co.envelope, 3)) AS measured_height " +
+		"FROM CITYOBJECT co, BUILDING b, SURFACE_GEOMETRY sg " +
+		"WHERE " +
+			"co.gmlid = ? " +
+		"AND b.id = co.id " +
+		"AND sg.root_id = b.lod4_geometry_id " +
+		"AND sg.geometry IS NOT NULL " +
+		"GROUP BY b.id " +
+		"ORDER BY b.id";
+
+	private static final String QUERY_EXTRUDED_LOD4_GET_BUILDING_DATA =
+		"SELECT SDO_CS.TRANSFORM(sg.geometry, 4326), b.measured_height " +
+		"FROM SURFACE_GEOMETRY sg, THEMATIC_SURFACE ts, CITYOBJECT co, BUILDING b " +
+		"WHERE " +
+			"co.gmlid = ? " +
+			"AND b.id = co.id " +
+			"AND ts.building_id = co.id " +
+			"AND ts.type = 'GroundSurface' " +
+			"AND sg.root_id = ts.lod4_multi_surface_id " +
+			"AND sg.geometry IS NOT NULL " +
+		"ORDER BY ts.building_id";
+
+	private static final String QUERY_GEOMETRY_LOD4_GET_BUILDING_DATA_ALT =
+		"SELECT sg.geometry, ts.type " +
+		"FROM CITYOBJECT co, BUILDING b, SURFACE_GEOMETRY sg " +
+			"LEFT JOIN THEMATIC_SURFACE ts ON ts.building_id = co.id " +
+		"WHERE " +
+			"co.gmlid = ? " +
+		"AND b.id = co.id " +
+		"AND sg.root_id = b.lod4_geometry_id " +
+		"AND sg.geometry IS NOT NULL " +
+		"ORDER BY b.id, ts.type";
+
+	private static final String QUERY_GEOMETRY_LOD4_GET_BUILDING_DATA =
+		"SELECT sg.geometry, ts.type " +
+		"FROM SURFACE_GEOMETRY sg, THEMATIC_SURFACE ts, CITYOBJECT co " +
+		"WHERE " +
+			"co.gmlid = ? " +
+			"AND ts.building_id = co.id " +
+			"AND sg.root_id = ts.lod4_multi_surface_id " +
+			"AND sg.geometry IS NOT NULL " +
+		"ORDER BY ts.building_id, ts.type";
+
+	private static final String QUERY_COLLADA_LOD4_GET_BUILDING_ROOT_SURFACES =
+		"SELECT b.lod4_geometry_id " + 
+		"FROM CITYOBJECT co, BUILDING b " + 
+		"WHERE " +  
+			"co.gmlid = ? " +
+			"AND b.id = co.id " +
+			"AND b.lod4_geometry_id IS NOT NULL " +
+		"UNION " + 
+		"SELECT ts.lod4_multi_surface_id " + 
+		"FROM CITYOBJECT co, THEMATIC_SURFACE ts " + 
+		"WHERE " +  
+			"co.gmlid = ? " +
+			"AND ts.building_id = co.id " +
+			"AND ts.lod4_multi_surface_id IS NOT NULL";
+	
 	private static final String QUERY_FOOTPRINT_LOD3_GET_BUILDING_DATA_ALT =
 		"SELECT SDO_CS.TRANSFORM(SDO_AGGR_UNION(SDOAGGRTYPE(sg.geometry, 0.05)), 4326), " +
 				"MAX(b.id) AS building_id " +
@@ -271,6 +354,26 @@ public class TileQueries {
 					"AND ts.building_id = co.id " +
 					"AND ts.lod3_multi_surface_id IS NOT NULL)";
 
+	private static final String QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD4_GET_BUILDING_DATA =
+		"SELECT sg.geometry " +
+		"FROM SURFACE_GEOMETRY sg " +
+		"WHERE " +
+			"sg.geometry IS NOT NULL " +
+			"AND sg.root_id IN (" +
+				"SELECT b.lod4_geometry_id " + 
+				"FROM CITYOBJECT co, BUILDING b " + 
+				"WHERE " +  
+					"co.gmlid = ? " +
+					"AND b.id = co.id " +
+					"AND b.lod4_geometry_id IS NOT NULL " +
+				"UNION " + 
+				"SELECT ts.lod4_multi_surface_id " + 
+				"FROM CITYOBJECT co, THEMATIC_SURFACE ts " + 
+				"WHERE " +  
+					"co.gmlid = ? " +
+					"AND ts.building_id = co.id " +
+					"AND ts.lod4_multi_surface_id IS NOT NULL)";
+
 	public static final String QUERY_GET_STRVAL_GENERICATTRIB_FROM_GML_ID =
 		"SELECT coga.strval " +
 		"FROM CITYOBJECT co " + 
@@ -284,6 +387,23 @@ public class TileQueries {
 	public static final String TRANSFORM_GEOMETRY_TO_WGS84 =
 		"SELECT SDO_CS.TRANSFORM(?, 4326) FROM DUAL";
 
+
+    private static final HashMap<Integer, String> singleBuildingQueriesLod4Alt = new HashMap<Integer, String>();
+    static {
+    	singleBuildingQueriesLod4Alt.put(DisplayLevel.FOOTPRINT, QUERY_FOOTPRINT_LOD4_GET_BUILDING_DATA_ALT);
+    	singleBuildingQueriesLod4Alt.put(DisplayLevel.EXTRUDED, QUERY_EXTRUDED_LOD4_GET_BUILDING_DATA_ALT);
+    	singleBuildingQueriesLod4Alt.put(DisplayLevel.GEOMETRY, QUERY_GEOMETRY_LOD4_GET_BUILDING_DATA_ALT);
+// dummy, currently there is no alternative query for collada
+    	singleBuildingQueriesLod4Alt.put(DisplayLevel.COLLADA, QUERY_COLLADA_LOD4_GET_BUILDING_ROOT_SURFACES);
+    }
+
+    private static final HashMap<Integer, String> singleBuildingQueriesLod4 = new HashMap<Integer, String>();
+    static {
+    	singleBuildingQueriesLod4.put(DisplayLevel.FOOTPRINT, QUERY_FOOTPRINT_LOD4_GET_BUILDING_DATA);
+    	singleBuildingQueriesLod4.put(DisplayLevel.EXTRUDED, QUERY_EXTRUDED_LOD4_GET_BUILDING_DATA);
+    	singleBuildingQueriesLod4.put(DisplayLevel.GEOMETRY, QUERY_GEOMETRY_LOD4_GET_BUILDING_DATA);
+    	singleBuildingQueriesLod4.put(DisplayLevel.COLLADA, QUERY_COLLADA_LOD4_GET_BUILDING_ROOT_SURFACES);
+    }
 
     private static final HashMap<Integer, String> singleBuildingQueriesLod3Alt = new HashMap<Integer, String>();
     static {
@@ -338,28 +458,11 @@ public class TileQueries {
     		case 3:
     	    	query = singleBuildingQueriesLod3.get(displayLevel.getLevel());
     	    	break;
+    		case 4:
+    	    	query = singleBuildingQueriesLod4.get(displayLevel.getLevel());
+    	    	break;
     	    default:
     	    	Logger.getInstance().log(LogLevelType.INFO, "No single building query found for LoD" + lodToExportFrom);
-    	}
-    	
-//    	Logger.getInstance().log(LogLevelType.DEBUG, query);
-    	return query;
-    }
-
-    public static String getSingleBuildingHighlightingQuery (int lodToExportFrom) {
-    	String query = null;
-    	switch (lodToExportFrom) {
-    		case 1:
-    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD1_GET_BUILDING_DATA;
-    	    	break;
-    		case 2:
-    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD2_GET_BUILDING_DATA;
-    			break;
-    		case 3:
-    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD3_GET_BUILDING_DATA;
-    	    	break;
-    	    default:
-    	    	Logger.getInstance().log(LogLevelType.INFO, "No single building highlighting query found for LoD" + lodToExportFrom);
     	}
     	
 //    	Logger.getInstance().log(LogLevelType.DEBUG, query);
@@ -379,8 +482,34 @@ public class TileQueries {
     		case 3:
     	    	query = singleBuildingQueriesLod3Alt.get(displayLevel.getLevel());
     	    	break;
+    		case 4:
+    	    	query = singleBuildingQueriesLod4Alt.get(displayLevel.getLevel());
+    	    	break;
     	    default:
     	    	Logger.getInstance().log(LogLevelType.INFO, "No alternative single building query found for LoD" + lodToExportFrom);
+    	}
+    	
+//    	Logger.getInstance().log(LogLevelType.DEBUG, query);
+    	return query;
+    }
+
+    public static String getSingleBuildingHighlightingQuery (int lodToExportFrom) {
+    	String query = null;
+    	switch (lodToExportFrom) {
+    		case 1:
+    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD1_GET_BUILDING_DATA;
+    	    	break;
+    		case 2:
+    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD2_GET_BUILDING_DATA;
+    			break;
+    		case 3:
+    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD3_GET_BUILDING_DATA;
+    	    	break;
+    		case 4:
+    			query = QUERY_GEOMETRY_FOR_HIGHLIGHTING_LOD4_GET_BUILDING_DATA;
+    	    	break;
+    	    default:
+    	    	Logger.getInstance().log(LogLevelType.INFO, "No single building highlighting query found for LoD" + lodToExportFrom);
     	}
     	
 //    	Logger.getInstance().log(LogLevelType.DEBUG, query);
