37e3d8dd78285a438c9d073cc840de377945b4ef
Claus Nagel <cnagel@virtualcitysystems.de>
2011-05-13 06:15:51 +0000

    fixes issue #23

--- src/de/tub/citydb/controller/KmlExporter.java
+++ src/de/tub/citydb/controller/KmlExporter.java
@@ -178,6 +178,22 @@ public class KmlExporter implements EventListener {
 			} else 
 				Logger.getInstance().info("Switching to database workspace " + name + '.');
 		}
+		
+		// check whether spatial indexes are enabled
+		Logger.getInstance().info("Checking for spatial indexes on geometry columns of involved tables...");
+		DBUtil dbUtil = DBUtil.getInstance(dbPool);
+		try {
+			if (!dbUtil.isIndexed("CITYOBJECT", "ENVELOPE") || 
+					!dbUtil.isIndexed("SURFACE_GEOMETRY", "GEOMETRY")) {
+				Logger.getInstance().error("Spatial indexes are not activated.");
+				Logger.getInstance().error("Please use the preferences tab to activate the spatial indexes.");
+				return false;
+			}
+
+		} catch (SQLException e) {
+			Logger.getInstance().error("Failed to retrieve status of spatial indexes: " + e.getMessage());
+			return false;
+		}
 
 		// create a saxWriter instance 
 		// define indent for xml output and namespace mappings
