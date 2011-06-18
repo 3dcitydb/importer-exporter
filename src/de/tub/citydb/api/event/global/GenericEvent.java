c7bed18782ddb58921b3e21a05ac1a46e8423df8
Claus Nagel <cnagel@virtualcitysystems.de>
2011-06-18 05:56:08 +0000

    minor change to GenericEvent

--- src/de/tub/citydb/api/event/global/GenericEvent.java
+++ src/de/tub/citydb/api/event/global/GenericEvent.java
@@ -7,32 +7,32 @@ import de.tub.citydb.api.event.Event;
 
 public final class GenericEvent extends Event {
 	private final String id;
-	private final HashMap<String, Object> properties;
-	
-	public GenericEvent(String id, Object source) {
-		super(GlobalEvents.GENERIC_EVENT, source);
-		this.id = id;
-		properties = new HashMap<String, Object>();
-	}
-	
+	private HashMap<String, Object> properties;
+
 	public GenericEvent(String id, Map<String, Object> properties, Object source) {
 		super(GlobalEvents.GENERIC_EVENT, source);
 		this.id = id;
-		this.properties = new HashMap<String, Object>(properties);
+
+		if (properties != null)
+			this.properties = new HashMap<String, Object>(properties);
 	}
 	
+	public GenericEvent(String id, Object source) {
+		this(id, null, source);
+	}
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public boolean hasProperties() {
 		return !properties.isEmpty();
 	}
-	
+
 	public Map<String, Object> getProperties() {
 		return properties;
 	}
-	
+
 	public Object getProperty(String key) {
 		return hasProperties() ? properties.get(key) : null;
 	}
