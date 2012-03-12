5e37c2c49a92d7bdf26cd02401d00c2f366d8c6d
Claus Nagel <cnagel@virtualcitysystems.de>
2012-03-12 21:58:01 +0000

    [postgis] minor changes to UTIL.sql

--- resources/3dcitydb/postgis/PL_SQL/GEODB_PKG/UTIL/UTIL.sql
+++ resources/3dcitydb/postgis/PL_SQL/GEODB_PKG/UTIL/UTIL.sql
@@ -5,7 +5,7 @@
 -- Conversion:	Felix Kunde <felix-kunde@gmx.de>
 --
 -- Copyright:   (c) 2007-2012  Institute for Geodesy and Geoinformation Science,
---                             Technische Universität Berlin, Germany
+--                             Technische Universitaet Berlin, Germany
 --                             http://www.igg.tu-berlin.de
 --
 --              This skript is free software under the LGPL Version 2.1.
@@ -27,81 +27,30 @@
 --
 
 /*****************************************************************
-* TYPE DB_INFO_OBJ
-* 
-* global type to store information relevant to reference systems
-******************************************************************/
-
-CREATE TYPE geodb_pkg.DB_INFO_OBJ AS (
-  SRID NUMERIC,
-  GML_SRS_NAME VARCHAR(1000),
-  COORD_REF_SYS_NAME VARCHAR(2048),
-  COORD_REF_SYS_KIND VARCHAR(2048)
-  --VERSIONING VARCHAR(100)
-);
-
-/*
-CREATE TABLE geodb_pkg.DB_INFO_TABLE(
-  info_obj				geodb_pkg.DB_INFO_OBJ
-);
-*/
- 
-/* ****************************************************************
 * versioning_table
 *
 * @param table_name name of the unversioned table, i.e., omit
 *                   suffixes such as _LT
 * @RETURN VARCHAR 'ON' for version-enabled, 'OFF' otherwise
 ******************************************************************/
-/*
-CREATE FUNCTION geodb_pkg.util_versioning_table(table_name VARCHAR) RETURNS VARCHAR AS $$
-  DECLARE
-    status USER_TABLES.STATUS%TYPE;
-	tablename ALIAS FOR $1;
-  BEGIN
-    EXECUTE 'SELECT STATUS FROM USER_TABLES WHERE TABLE_NAME=$1' INTO status USING tablename || '_LT';
-    RETURN 'ON';
-  EXCEPTION
-    WHEN others THEN
-      RETURN 'OFF';
-  END;
+CREATE OR REPLACE FUNCTION geodb_pkg.util_versioning_table(table_name VARCHAR) RETURNS VARCHAR AS $$
+BEGIN
+    RETURN 'OFF';
+END;
 $$
 LANGUAGE plpgsql;
-*/
+
 /*****************************************************************
 * versioning_db
 *
 * @RETURN VARCHAR 'ON' for version-enabled, 'PARTLY' and 'OFF'
 ******************************************************************/
-/*
-CREATE FUNCTION geodb_pkg.util_versioning_db() RETURNS VARCHAR AS $$
-  DECLARE
-    table_names text[];
-    is_versioned BOOLEAN DEFAULT FALSE;
-    not_versioned BOOLEAN DEFAULT FALSE;
-  BEGIN
-    table_names := string_to_array('ADDRESS,ADDRESS_TO_BUILDING,APPEAR_TO_SURFACE_DATA,APPEARANCE,BREAKLINE_RELIEF,BUILDING,BUILDING_FURNITURE,BUILDING_INSTALLATION,CITY_FURNITURE,CITYMODEL,CITYOBJECT,CITYOBJECT_GENERICATTRIB,CITYOBJECT_MEMBER,CITYOBJECTGROUP,EXTERNAL_REFERENCE,GENERALIZATION,GENERIC_CITYOBJECT,GROUP_TO_CITYOBJECT,IMPLICIT_GEOMETRY,LAND_USE,MASSPOINT_RELIEF,OPENING,OPENING_TO_THEM_SURFACE,PLANT_COVER,RELIEF_COMPONENT,RELIEF_FEAT_TO_REL_COMP,RELIEF_FEATURE,ROOM,SOLITARY_VEGETAT_OBJECT,SURFACE_DATA,SURFACE_GEOMETRY,TEXTUREPARAM,THEMATIC_SURFACE,TIN_RELIEF,TRAFFIC_AREA,TRANSPORTATION_COMPLEX,WATERBOD_TO_WATERBND_SRF,WATERBODY,WATERBOUNDARY_SURFACE');
-  
-    FOR i IN array_lower(table_names, 1) .. array_upper(table_names, 2) LOOP
-      IF versioning_table(table_names[i]) = 'ON' THEN
-        is_versioned := TRUE;
-      ELSE
-        not_versioned := TRUE;
-      END IF;
-      
-    END LOOP;
-
-    IF is_versioned AND NOT not_versioned THEN
-      RETURN 'ON';
-    ELSIF is_versioned AND not_versioned THEN
-      RETURN 'PARTLY';
-    ELSE
-      RETURN 'OFF';
-    END IF;
-  END;
+CREATE OR REPLACE FUNCTION geodb_pkg.util_versioning_db() RETURNS VARCHAR AS $$
+BEGIN
+    RETURN 'OFF';
+END;
 $$
 LANGUAGE plpgsql;
-*/
  
 /*****************************************************************
 * db_info
@@ -110,46 +59,45 @@ LANGUAGE plpgsql;
 * @param srs database srs name
 * @param versioning database versioning
 ******************************************************************/
-
-  CREATE FUNCTION geodb_pkg.util_db_info(OUT srid DATABASE_SRS.SRID%TYPE, OUT srs DATABASE_SRS.GML_SRS_NAME%TYPE/*, OUT versioning VARCHAR*/) RETURNS SETOF record AS $$
+CREATE OR REPLACE FUNCTION geodb_pkg.util_db_info(OUT srid DATABASE_SRS.SRID%TYPE, OUT srs DATABASE_SRS.GML_SRS_NAME%TYPE) RETURNS SETOF record AS $$
     SELECT srid, gml_srs_name FROM database_srs;
-	--versioning := versioning_db();
-  $$ LANGUAGE sql;
+$$ 
+LANGUAGE sql;
 
 /******************************************************************
 * db_metadata
 *
 ******************************************************************/
-  
-  CREATE FUNCTION geodb_pkg.util_db_metadata() 
-  RETURNS TABLE(srid INTEGER, gml_srs_name VARCHAR(1000), coord_ref_sys_name VARCHAR(2048), coord_ref_sys_kind VARCHAR(2048)/*, VERSIONING VARCHAR(100)*/) AS $$
-  BEGIN
+CREATE OR REPLACE FUNCTION geodb_pkg.util_db_metadata() 
+RETURNS TABLE(srid INTEGER, gml_srs_name VARCHAR(1000), coord_ref_sys_name VARCHAR(2048), coord_ref_sys_kind VARCHAR(2048)) AS $$
+BEGIN
     EXECUTE 'SELECT SRID, GML_SRS_NAME FROM DATABASE_SRS' INTO srid, gml_srs_name;
-	EXECUTE 'SELECT srtext, srtext FROM spatial_ref_sys WHERE SRID=' || srid || '' INTO coord_ref_sys_name, coord_ref_sys_kind;
-    --info_tmp.versioning := versioning_db;
+    EXECUTE 'SELECT srtext, srtext FROM spatial_ref_sys WHERE SRID=' || srid || '' INTO coord_ref_sys_name, coord_ref_sys_kind;
     coord_ref_sys_name := split_part(coord_ref_sys_name, '"', 2);
-	coord_ref_sys_kind := split_part(coord_ref_sys_kind, '[', 1);
-	RETURN NEXT;
-  END;
-  $$
-  LANGUAGE plpgsql;
-  
+    coord_ref_sys_kind := split_part(coord_ref_sys_kind, '[', 1);
+    RETURN NEXT;
+END;
+$$
+LANGUAGE plpgsql;  
   
 /******************************************************************
 * error_msg
 *
-* @param err_code Oracle SQL error code, usually starting with '-',
-*                 e.g. '-06404'
-* @RETURN VARCHAR corresponding Oracle SQL error message                 
+* @param err_code PostgreSQL error code, e.g. '06404'
+* @RETURN VARCHAR corresponding PostgreSQL error message                 
 ******************************************************************/
-
-  CREATE FUNCTION geodb_pkg.util_error_msg(err_code VARCHAR) RETURNS void AS $$
-  BEGIN
-    RAISE EXCEPTION USING ERRCODE = err_code;
-	RETURN;
-  END;
-  $$
-  LANGUAGE plpgsql IMMUTABLE;
+CREATE OR REPLACE FUNCTION geodb_pkg.util_error_msg(err_code VARCHAR) RETURNS VARCHAR AS $$
+BEGIN
+    BEGIN
+        RAISE EXCEPTION USING ERRCODE = err_code;
+        RETURN err_code;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RETURN SQLERRM;
+   END;
+END;
+$$
+LANGUAGE plpgsql;
    
 /******************************************************************
 * min
@@ -158,17 +106,16 @@ LANGUAGE plpgsql;
 * @param b second NUMERIC value
 * @RETURN NUMERIC the smaller of the two input NUMERIC values                
 ******************************************************************/
-
-CREATE FUNCTION geodb_pkg.util_min(a NUMERIC, b NUMERIC) RETURNS NUMERIC AS $$
-  BEGIN
+CREATE OR REPLACE FUNCTION geodb_pkg.util_min(a NUMERIC, b NUMERIC) RETURNS NUMERIC AS $$
+BEGIN
     IF a < b THEN
-      RETURN a;
+        RETURN a;
     ELSE
-      RETURN b;
+        RETURN b;
     END IF;
-  END;
+END;
 $$
-LANGUAGE plpgsql IMMUTABLE;
+LANGUAGE plpgsql;
   
 /******************************************************************
 * transform_or_null
@@ -177,19 +124,17 @@ LANGUAGE plpgsql IMMUTABLE;
 * @param srid the SRID of the coordinate system to be used for the transformation.
 * @RETURN GEOMETRY the transformed geometry representation                
 ******************************************************************/
-
-CREATE FUNCTION geodb_pkg.util_transform_or_null(geom GEOMETRY, srid INTEGER) RETURNS geometry AS $$
-  BEGIN
+CREATE OR REPLACE FUNCTION geodb_pkg.util_transform_or_null(geom GEOMETRY, srid INTEGER) RETURNS geometry AS $$
+BEGIN
     IF geom IS NOT NULL THEN
-      RETURN ST_Transform(geom, srid);
+        RETURN ST_Transform(geom, srid);
     ELSE
-      RETURN NULL;
+        RETURN NULL;
     END IF;
-  END;
+END;
 $$
 LANGUAGE plpgsql;
-  
-  
+    
 /******************************************************************
 * is_coord_ref_sys_3d
 *
