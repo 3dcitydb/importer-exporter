e6529116a64a44ef4d9fac318ed75a9c084f9126
Claus Nagel <cnagel@virtualcitysystems.de>
2012-03-25 11:34:09 +0000

    minor change to DBReliefFeature.java

--- src/de/tub/citydb/modules/citygml/importer/database/content/DBReliefFeature.java
+++ src/de/tub/citydb/modules/citygml/importer/database/content/DBReliefFeature.java
@@ -34,6 +34,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
+import org.citygml4j.model.citygml.CityGMLClass;
 import org.citygml4j.model.citygml.relief.AbstractReliefComponent;
 import org.citygml4j.model.citygml.relief.ReliefComponentProperty;
 import org.citygml4j.model.citygml.relief.ReliefFeature;
@@ -46,14 +47,14 @@ import de.tub.citydb.util.Util;
 
 public class DBReliefFeature implements DBImporter {
 	private final Logger LOG = Logger.getInstance();
-	
+
 	private final Connection batchConn;
 	private final DBImporterManager dbImporterManager;
 
 	private PreparedStatement psReliefFeature;
 	private DBCityObject cityObjectImporter;
 	private DBReliefComponent reliefComponentImporter;
-	
+
 	private int batchCounter;
 
 	public DBReliefFeature(Connection batchConn, DBImporterManager dbImporterManager) throws SQLException {
@@ -62,7 +63,7 @@ public class DBReliefFeature implements DBImporter {
 
 		init();
 	}
-	
+
 	private void init() throws SQLException {
 		psReliefFeature = batchConn.prepareStatement("insert into RELIEF_FEATURE (ID, NAME, NAME_CODESPACE, DESCRIPTION, LOD) values " +
 				"(?, ?, ?, ?, ?)");
@@ -70,7 +71,7 @@ public class DBReliefFeature implements DBImporter {
 		cityObjectImporter = (DBCityObject)dbImporterManager.getDBImporter(DBImporterEnum.CITYOBJECT);
 		reliefComponentImporter = (DBReliefComponent)dbImporterManager.getDBImporter(DBImporterEnum.RELIEF_COMPONENT);
 	}
-	
+
 	public long insert(ReliefFeature reliefFeature) throws SQLException {
 		long reliefFeatureId = dbImporterManager.getDBId(DBSequencerEnum.CITYOBJECT_SEQ);
 		boolean success = false;
@@ -83,10 +84,10 @@ public class DBReliefFeature implements DBImporter {
 		else
 			return 0;
 	}
-	
+
 	private boolean insert(ReliefFeature reliefFeature, long reliefFeatureId) throws SQLException {
 		String origGmlId = reliefFeature.getId();
-		
+
 		// CityObject
 		long cityObjectId = cityObjectImporter.insert(reliefFeature, reliefFeatureId, true);
 		if (cityObjectId == 0)
@@ -106,7 +107,7 @@ public class DBReliefFeature implements DBImporter {
 			psReliefFeature.setNull(2, Types.VARCHAR);
 			psReliefFeature.setNull(3, Types.VARCHAR);
 		}
-		
+
 		// gml:description
 		if (reliefFeature.isSetDescription()) {
 			String description = reliefFeature.getDescription().getValue();
@@ -118,56 +119,66 @@ public class DBReliefFeature implements DBImporter {
 		} else {
 			psReliefFeature.setNull(4, Types.VARCHAR);
 		}
-		
+
 		// lod
 		psReliefFeature.setInt(5, reliefFeature.getLod());
-		
+
 		psReliefFeature.addBatch();
 		if (++batchCounter == Internal.ORACLE_MAX_BATCH_SIZE)
 			dbImporterManager.executeBatch(DBImporterEnum.RELIEF_FEATURE);
-		
+
 		// relief component
 		if (reliefFeature.isSetReliefComponent()) {
 			for (ReliefComponentProperty property : reliefFeature.getReliefComponent()) {
 				AbstractReliefComponent component = property.getReliefComponent();
-				
+
 				if (component != null) {
 					String gmlId = component.getId();
-					long id = reliefComponentImporter.insert(component, reliefFeatureId);
-					
-					if (id == 0) {
+
+					if (component.getCityGMLClass() != CityGMLClass.RASTER_RELIEF) {
+						long id = reliefComponentImporter.insert(component, reliefFeatureId);
+
+						if (id == 0) {
+							StringBuilder msg = new StringBuilder(Util.getFeatureSignature(
+									reliefFeature.getCityGMLClass(), 
+									origGmlId));
+							msg.append(": Failed to write ");
+							msg.append(Util.getFeatureSignature(
+									component.getCityGMLClass(), 
+									gmlId));
+
+							LOG.error(msg.toString());
+						}
+					} else {
 						StringBuilder msg = new StringBuilder(Util.getFeatureSignature(
-								reliefFeature.getCityGMLClass(), 
-								origGmlId));
-						msg.append(": Failed to write ");
-						msg.append(Util.getFeatureSignature(
 								component.getCityGMLClass(), 
 								gmlId));
-						
+						msg.append(": Raster relief components are not supported.");
+
 						LOG.error(msg.toString());
 					}
-					
+
 					// free memory of nested feature
 					property.unsetReliefComponent();
 				} else {
 					// xlink
-        			String href = property.getHref();
+					String href = property.getHref();
 
-        			if (href != null && href.length() != 0) {
-        				dbImporterManager.propagateXlink(new DBXlinkBasic(
-        						reliefFeatureId,
-        						TableEnum.RELIEF_FEATURE,
-        						href,
-        						TableEnum.RELIEF_COMPONENT
-        				));
-        			}
+					if (href != null && href.length() != 0) {
+						dbImporterManager.propagateXlink(new DBXlinkBasic(
+								reliefFeatureId,
+								TableEnum.RELIEF_FEATURE,
+								href,
+								TableEnum.RELIEF_COMPONENT
+								));
+					}
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public void executeBatch() throws SQLException {
 		psReliefFeature.executeBatch();
