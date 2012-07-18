754d632cb9021851e06bfa9f2b4e477ecf0f32ed
Felix Kunde <fkunde@virtualcitysystems.de>
2012-07-18 09:19:02 +0000

    [postgis] removed unknown letter from DROP_DB.sql

--- resources/3dcitydb/postgis/DROP_DB.sql
+++ resources/3dcitydb/postgis/DROP_DB.sql
@@ -8,7 +8,7 @@
 -- Conversion:	Felix Kunde <felix-kunde@gmx.de>
 --
 -- Copyright:   (c) 2007-2012  Institute for Geodesy and Geoinformation Science,
---                             Technische Universit�t Berlin, Germany
+--                             Technische Universitaet Berlin, Germany
 --                             http://www.igg.tu-berlin.de
 --
 --              This skript is free software under the LGPL Version 2.1.
