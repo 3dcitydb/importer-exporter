429f4a3ab265116a5ed014b9ab3a36af4039e9a6
György Hudra <ghudra@moss.de>
2013-10-24 15:47:56 +0000

    1.6-b1: config. for creationDate, terminationDate

---
+++ src/de/tub/citydb/config/project/importer/CreationDateMode.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the 3D City Database Importer/Exporter.
+ * Copyright (c) 2007 - 2013
+ * Institute for Geodesy and Geoinformation Science
+ * Technische Universitaet Berlin, Germany
+ * http://www.gis.tu-berlin.de/
+ * 
+ * The 3D City Database Importer/Exporter program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * The development of the 3D City Database Importer/Exporter has 
+ * been financially supported by the following cooperation partners:
+ * 
+ * Business Location Center, Berlin <http://www.businesslocationcenter.de/>
+ * virtualcitySYSTEMS GmbH, Berlin <http://www.virtualcitysystems.de/>
+ * Berlin Senate of Business, Technology and Women <http://www.berlin.de/sen/wtf/>
+ */
+package de.tub.citydb.config.project.importer;
+
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name="CreationDateModeType")
+public enum CreationDateMode {
+	@XmlEnumValue("inherit")
+	INHERIT("inherit"),
+	@XmlEnumValue("complement")
+	COMPLEMENT("complement"),
+	@XmlEnumValue("replace")
+	REPLACE("replace");
+
+	private final String value;
+
+	CreationDateMode(String v) {
+		value = v;
+	}
+
+	public String value() {
+		return value;
+	}
+
+	public static CreationDateMode fromValue(String v) {
+		for (CreationDateMode c: CreationDateMode.values()) {
+			if (c.value.equals(v)) {
+				return c;
+			}
+		}
+
+		return REPLACE;
+	}
+}
