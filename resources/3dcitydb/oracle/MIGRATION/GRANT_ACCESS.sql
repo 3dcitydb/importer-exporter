a156a8e46b19cfe2374349430982799c754447a2
Felix Kunde <fkunde@virtualcitysystems.de>
2015-01-23 15:06:18 +0100

    reflect lates changes of database scripts from 3dcitydb repo

--- resources/3dcitydb/oracle/MIGRATION/GRANT_ACCESS.sql
+++ resources/3dcitydb/oracle/MIGRATION/GRANT_ACCESS.sql
@@ -1,14 +1,38 @@
+-- GRANT_ACCESS.sql
+--
+-- Author:     Arda Muftuoglu <amueftueoglu@moss.de>
+--             Felix Kunde <fkunde@virtualcitysystems.de>
+--
+-- Copyright:  (c) 2012-2014  Chair of Geoinformatics,
+--                            Technische Universität München, Germany
+--                            http://www.gis.bv.tum.de
+--
+--              This script is free software under the LGPL Version 2.1.
+--              See the GNU Lesser General Public License at
+--              http://www.gnu.org/copyleft/lgpl.html
+--              for more details.
+-------------------------------------------------------------------------------
+-- About:
+-- Script to grant reading access on a 3DCityDB instance of v2.1.0 to a user 
+-- with an installed version of the 3DCityDB v3.0.0
+-------------------------------------------------------------------------------
+--
+-- ChangeLog:
+--
+-- Version | Date       | Description                               | Author
+-- 1.0.0     2015-01-22   release version                             AM
+--                                                                    FKun
+--
+
 SET SERVEROUTPUT ON;
-ACCEPT SCHEMAINPUTUSER PROMPT 'Enter the username for the accesses to be granted: '
-ACCEPT SCHEMAINPUTSRC PROMPT 'Enter the schema name on which the accesses to be granted: ' 
+ACCEPT SCHEMAINPUT PROMPT 'Enter the user name of 3DCityDB v3.0.0 instance : '
 
 DECLARE
-  	schema_name_user VARCHAR2(30) := '&SCHEMAINPUTUSER';
-	schema_name_source VARCHAR2(30) := '&SCHEMAINPUTSRC';	
+    schema_name_user VARCHAR2(30) := upper('&SCHEMAINPUT');
 BEGIN
-	FOR R IN (SELECT owner, table_name FROM all_tables WHERE owner=schema_name_source) LOOP
-		EXECUTE IMMEDIATE 'grant select on "'||R.owner||'".'||R.table_name||' to "'||schema_name_user||'"';
-		dbms_output.put_line('grant select on "'||R.owner||'".'||R.table_name||' to "'||schema_name_user||'"');
+	FOR R IN (SELECT table_name FROM user_tables) LOOP
+		EXECUTE IMMEDIATE 'grant select on '||R.table_name||' to "'||schema_name_user||'"';
+		dbms_output.put_line('grant select on '||R.table_name||' to "'||schema_name_user||'"');
 	END LOOP;
 END;
 /
