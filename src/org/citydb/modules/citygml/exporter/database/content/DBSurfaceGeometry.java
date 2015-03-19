504a30575f395a4a5c3887b6b2ae4c2488b6b3bf
Claus Nagel <cnagel@virtualcitysystems.de>
2015-03-19 23:24:43 +0100

    [citygml export] fixed issue with global appearance export

--- src/org/citydb/modules/citygml/exporter/database/content/DBSurfaceGeometry.java
+++ src/org/citydb/modules/citygml/exporter/database/content/DBSurfaceGeometry.java
@@ -107,7 +107,7 @@ public class DBSurfaceGeometry implements DBExporter {
 			if (dbExporterManager.getDatabaseAdapter().getSQLAdapter().requiresPseudoTableInSelect())
 				query.append("from ").append(dbExporterManager.getDatabaseAdapter().getSQLAdapter().getPseudoTableName()).append(" ");
 
-			query.append("where exists (select 1 from TEXTUREPARAM where SURFACE_GEOMETRY_ID = ? limit 1)");
+			query.append("where exists (select 1 from TEXTUREPARAM where SURFACE_GEOMETRY_ID = ?)");
 			psImportGmlId = tempTable.getConnection().prepareStatement(query.toString());
 		}
 
