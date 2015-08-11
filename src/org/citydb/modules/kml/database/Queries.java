d74772bd41fc1a58b15bab501f5057091c152bb2
Felix Kunde <fkunde@virtualcitysystems.de>
2015-08-11 13:24:16 +0200

    [kml-export] lod0 geometries are now exported correctly when using ROOFPRINT_PRIOR_FOOTPRINT mode

--- src/org/citydb/modules/kml/database/Queries.java
+++ src/org/citydb/modules/kml/database/Queries.java
@@ -632,7 +632,7 @@ public class Queries {
 			"FROM (" +
 						"SELECT * FROM (" +
 						"SELECT * FROM (" +
-				    	"SELECT citydb_util.to_2d(sg.geometry, <2D_SRID>) AS simple_geom " +
+				    	"SELECT sg.geometry AS simple_geom " +
 				    	"FROM SURFACE_GEOMETRY sg " +
 				    	"WHERE " +
 				    	"sg.root_id IN( " +
@@ -663,7 +663,7 @@ public class Queries {
 			"FROM (" +
 			"SELECT * FROM (" +
 			"SELECT * FROM (" +
-				        "SELECT ST_Force2D(sg.geometry) AS simple_geom " +
+				        "SELECT sg.geometry AS simple_geom " +
 				        "FROM SURFACE_GEOMETRY sg " +
 				        "WHERE " +
 				        "sg.root_id IN( " +
@@ -710,7 +710,6 @@ public class Queries {
 		}
 		else {
 			return BUILDING_PART_GET_AGGREGATE_GEOMETRIES_FOR_LOD0(type).replace("<TOLERANCE>", String.valueOf(tolerance))
-					.replace("<2D_SRID>", String.valueOf(srid2D))
 					.replace("<GROUP_BY_1>", String.valueOf(groupBy1))
 					.replace("<GROUP_BY_2>", String.valueOf(groupBy2))
 					.replace("<GROUP_BY_3>", String.valueOf(groupBy3));					
