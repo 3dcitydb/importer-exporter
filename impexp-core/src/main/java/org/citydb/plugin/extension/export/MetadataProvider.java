ab706490481cf850bb251f2056588553db6a7f5b
Claus Nagel <cnagel@virtualcitysystems.de>
2019-02-11 20:40:50 +0100

    minor change

--- impexp-core/src/main/java/org/citydb/plugin/extension/export/MetadataProvider.java
+++
@@ -1,9 +0,0 @@
-package org.citydb.plugin.extension.export;
-
-import org.citydb.citygml.exporter.util.Metadata;
-import org.citydb.config.Config;
-import org.citydb.query.Query;
-
-public interface MetadataProvider {
-    Metadata getMetadata(Query query, Config config);
-}
