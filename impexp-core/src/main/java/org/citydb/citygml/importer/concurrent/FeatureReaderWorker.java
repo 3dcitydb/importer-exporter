ff614cf426d625a11a105bf7cf7417091c5477de
Zhihang Yao <zyao@virtualcitysystems.de>
2019-08-02 09:27:21 +0200

    FeatureReaderWorker is back

---
+++ impexp-core/src/main/java/org/citydb/citygml/importer/concurrent/FeatureReaderWorker.java
@@ -0,0 +1,110 @@
+/*
+ * 3D City Database - The Open Source CityGML Database
+ * http://www.3dcitydb.org/
+ *
+ * Copyright 2013 - 2019
+ * Chair of Geoinformatics
+ * Technical University of Munich, Germany
+ * https://www.gis.bgu.tum.de/
+ *
+ * The 3D City Database is jointly developed with the following
+ * cooperation partners:
+ *
+ * virtualcitySYSTEMS GmbH, Berlin <http://www.virtualcitysystems.de/>
+ * M.O.S.S. Computer Grafik Systeme GmbH, Taufkirchen <http://www.moss.de/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.citydb.citygml.importer.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.citydb.concurrent.Worker;
+import org.citydb.concurrent.WorkerPool;
+import org.citydb.config.Config;
+import org.citydb.config.project.global.LogLevel;
+import org.citydb.event.EventDispatcher;
+import org.citydb.event.global.InterruptEvent;
+import org.citydb.log.Logger;
+import org.citygml4j.model.citygml.CityGML;
+import org.citygml4j.xml.io.reader.MissingADESchemaException;
+import org.citygml4j.xml.io.reader.UnmarshalException;
+import org.citygml4j.xml.io.reader.XMLChunk;
+
+public class FeatureReaderWorker extends Worker<XMLChunk> {
+	private final Logger LOG = Logger.getInstance();
+	private final ReentrantLock runLock = new ReentrantLock();
+	private volatile boolean shouldRun = true;
+
+	private final WorkerPool<CityGML> dbWorkerPool;
+	private final EventDispatcher eventDispatcher;
+	private final boolean useValidation;
+
+	public FeatureReaderWorker(WorkerPool<CityGML> dbWorkerPool,
+			Config config,
+			EventDispatcher eventDispatcher) {
+		this.dbWorkerPool = dbWorkerPool;
+		this.eventDispatcher = eventDispatcher;
+
+		useValidation = config.getProject().getImporter().getXMLValidation().isSetUseXMLValidation();
+	}
+	
+	@Override
+	public void interrupt() {
+		shouldRun = false;
+	}
+
+	@Override
+	public void run() {
+		if (firstWork != null) {
+			doWork(firstWork);
+			firstWork = null;
+		}
+
+		while (shouldRun) {
+			try {
+				XMLChunk work = workQueue.take();				
+				doWork(work);
+			} catch (InterruptedException ie) {
+				// re-check state
+			}
+		}
+	}
+
+	private void doWork(XMLChunk work) {
+		final ReentrantLock runLock = this.runLock;
+		runLock.lock();
+
+		try {
+			try {
+				CityGML cityGML = work.unmarshal();
+				if (!useValidation || work.hasPassedXMLValidation())
+					dbWorkerPool.addWork(cityGML);
+			} catch (UnmarshalException e) {
+				if (!useValidation || work.hasPassedXMLValidation()) {
+					StringBuilder msg = new StringBuilder();				
+					msg.append("Failed to unmarshal XML chunk: ").append(e.getMessage());			
+					LOG.error(msg.toString());
+				}
+			} catch (MissingADESchemaException e) {
+				eventDispatcher.triggerEvent(new InterruptEvent("Failed to read an ADE XML Schema.", LogLevel.ERROR, e, eventChannel, this));
+			} catch (Exception e) {
+				// this is to catch general exceptions that may occur during the import
+				eventDispatcher.triggerEvent(new InterruptEvent("Aborting due to an unexpected " + e.getClass().getName() + " error.", LogLevel.ERROR, e, eventChannel, this));
+			}
+		} finally {
+			runLock.unlock();
+		}
+	}
+
+}
--- impexp-core/src/main/java/org/citydb/citygml/importer/reader/citygml/CityGMLReader.java
+++ impexp-core/src/main/java/org/citydb/citygml/importer/reader/citygml/CityGMLReader.java
@@ -1,7 +1,9 @@
 package org.citydb.citygml.importer.reader.citygml;
 
+import org.citydb.citygml.importer.concurrent.FeatureReaderWorkerFactory;
 import org.citydb.citygml.importer.reader.FeatureReadException;
 import org.citydb.citygml.importer.reader.FeatureReader;
+import org.citydb.concurrent.PoolSizeAdaptationStrategy;
 import org.citydb.concurrent.WorkerPool;
 import org.citydb.config.Config;
 import org.citydb.config.project.query.filter.counter.CounterFilter;
@@ -10,27 +12,24 @@ import org.citydb.event.EventDispatcher;
 import org.citydb.event.EventHandler;
 import org.citydb.event.global.EventType;
 import org.citydb.file.InputFile;
-import org.citydb.log.Logger;
 import org.citydb.registry.ObjectRegistry;
 import org.citygml4j.model.citygml.CityGML;
-import org.citygml4j.model.citygml.appearance.Appearance;
+import org.citygml4j.model.citygml.CityGMLClass;
 import org.citygml4j.xml.io.CityGMLInputFactory;
 import org.citygml4j.xml.io.reader.CityGMLInputFilter;
 import org.citygml4j.xml.io.reader.CityGMLReadException;
-import org.citygml4j.xml.io.reader.MissingADESchemaException;
-import org.citygml4j.xml.io.reader.UnmarshalException;
 import org.citygml4j.xml.io.reader.XMLChunk;
 
 import java.io.IOException;
 
 public class CityGMLReader implements FeatureReader, EventHandler {
-	private final Logger LOG = Logger.getInstance();
     private final CityGMLInputFilter typeFilter;
     private final CounterFilter counterFilter;
     private final ValidationErrorHandler validationHandler;
     private final CityGMLInputFactory factory;
     private final Config config;
     private final EventDispatcher eventDispatcher;
+    private final int minThreads, maxThreads;
 
     private volatile boolean shouldRun = true;
 
@@ -40,6 +39,9 @@ public class CityGMLReader implements FeatureReader, EventHandler {
         this.validationHandler = validationHandler;
         this.factory = factory;
         this.config = config;
+
+        minThreads = config.getProject().getImporter().getResources().getThreadPool().getDefaultPool().getMinThreads();
+        maxThreads = config.getProject().getImporter().getResources().getThreadPool().getDefaultPool().getMaxThreads();
 
         eventDispatcher = ObjectRegistry.getInstance().getEventDispatcher();
         eventDispatcher.addEventHandler(EventType.INTERRUPT,this);
@@ -55,57 +57,64 @@ public class CityGMLReader implements FeatureReader, EventHandler {
         if (validationHandler != null)
             validationHandler.reset();
 
+        WorkerPool<XMLChunk> featureWorkerPool = null;
         org.citygml4j.xml.io.reader.CityGMLReader reader;
 
         try {
-            reader = factory.createFilteredCityGMLReader(factory.createCityGMLReader(inputFile.getFile().toString(), inputFile.openStream()), typeFilter);
-            boolean useValidation = config.getProject().getImporter().getXMLValidation().isSetUseXMLValidation();
-            boolean breakTopLevelFeatureImport = false;
-            
-            while (shouldRun && reader.hasNext()) {
-                XMLChunk chunk = reader.nextChunk();
-                CityGML cityGML = null;
-                try {
-    				cityGML = chunk.unmarshal();
-    			} catch (UnmarshalException e) {
-    				if (!useValidation || chunk.hasPassedXMLValidation()) {
-    					StringBuilder msg = new StringBuilder();				
-    					msg.append("Failed to unmarshal XML chunk: ").append(e.getMessage());			
-    					LOG.error(msg.toString());
-    				}
-    			} catch (MissingADESchemaException e) {
-    				throw new FeatureReadException("Failed to read an ADE XML Schema.", e);
-    			} catch (Exception e) {
-    				// this is to catch general exceptions that may occur during the import
-    				throw new FeatureReadException("Aborting due to an unexpected " + e.getClass().getName() + " error.", e);
-    			}
-                
-				if (!useValidation || chunk.hasPassedXMLValidation()) {
-					if (counterFilter != null && !(cityGML instanceof Appearance)) {
+            // this worker pool unmarshals feature chunks and passes them to the database worker pool
+            featureWorkerPool = new WorkerPool<>(
+                    "citygml_parser_pool",
+                    minThreads,
+                    maxThreads,
+                    PoolSizeAdaptationStrategy.AGGRESSIVE,
+                    new FeatureReaderWorkerFactory(workerPool, config, eventDispatcher),
+                    maxThreads * 2,
+                    false);
+
+            featureWorkerPool.prestartCoreWorkers();
+
+            try {
+                reader = factory.createFilteredCityGMLReader(factory.createCityGMLReader(inputFile.getFile().toString(), inputFile.openStream()), typeFilter);
+                boolean breakTopLevelFeatureImport = false;
+
+                while (shouldRun && reader.hasNext()) {
+                    XMLChunk chunk = reader.nextChunk();
+                    CityGMLClass type = chunk.getCityGMLClass();
+
+                    if (counterFilter != null && type != CityGMLClass.APPEARANCE) {
                         counter++;
 
                         if (counter < counterFilter.getLowerLimit())
                             continue;
 
                         if (counter > counterFilter.getUpperLimit())
-                        	breakTopLevelFeatureImport = true;
+                            breakTopLevelFeatureImport = true;
                     }
-					
-					if (!breakTopLevelFeatureImport || (cityGML instanceof Appearance && breakTopLevelFeatureImport))
-						workerPool.addWork(cityGML);
-				}
+
+                    if (!breakTopLevelFeatureImport || type == CityGMLClass.APPEARANCE)
+                        featureWorkerPool.addWork(chunk);
+                }
+            } catch (CityGMLReadException | IOException e) {
+                throw new FeatureReadException("Failed to read CityGML input file.", e);
             }
-        } catch (CityGMLReadException | IOException e) {
-            throw new FeatureReadException("Failed to read CityGML input file.", e);
-        }
 
-        try {
-            reader.close();
-        } catch (CityGMLReadException e) {
-            throw new FeatureReadException("Failed to close CityGML reader.", e);
-        }
+            try {
+                featureWorkerPool.shutdownAndWait();
+            } catch (InterruptedException e) {
+                throw new FeatureReadException("Failed to shutdown CityGML feature reader pool.", e);
+            }
 
-        return counter;      
+            try {
+                reader.close();
+            } catch (CityGMLReadException e) {
+                throw new FeatureReadException("Failed to close CityGML reader.", e);
+            }
+
+            return counter;
+        } finally {
+            if (featureWorkerPool != null && !featureWorkerPool.isTerminated())
+                featureWorkerPool.shutdownNow();
+        }
     }
 
     @Override
