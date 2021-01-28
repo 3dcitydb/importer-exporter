2c4998ecf50515bfec68bfa64c3f7e5adf42fea1
Claus Nagel <cnagel@virtualcitysystems.de>
2021-01-29 00:37:58 +0100

    using citygml4j chunk-parser for CityJSON

--- impexp-core/src/main/java/org/citydb/citygml/importer/reader/cityjson/CityJSONReader.java
+++ impexp-core/src/main/java/org/citydb/citygml/importer/reader/cityjson/CityJSONReader.java
@@ -10,17 +10,12 @@ import org.citydb.event.EventHandler;
 import org.citydb.event.global.EventType;
 import org.citydb.file.InputFile;
 import org.citydb.registry.ObjectRegistry;
+import org.citygml4j.builder.cityjson.json.io.reader.CityJSONChunkReader;
 import org.citygml4j.builder.cityjson.json.io.reader.CityJSONInputFactory;
-import org.citygml4j.builder.cityjson.json.io.reader.CityJSONReadException;
 import org.citygml4j.model.citygml.CityGML;
 import org.citygml4j.model.citygml.appearance.Appearance;
-import org.citygml4j.model.citygml.core.CityModel;
 import org.citygml4j.model.gml.feature.AbstractFeature;
-import org.citygml4j.model.gml.feature.FeatureProperty;
 import org.citygml4j.xml.io.reader.CityGMLInputFilter;
-
-import java.io.IOException;
-import java.util.Iterator;
 
 public class CityJSONReader implements FeatureReader, EventHandler {
     private final CityGMLInputFilter typeFilter;
@@ -28,6 +23,7 @@ public class CityJSONReader implements FeatureReader, EventHandler {
     private final CityJSONInputFactory factory;
     private final EventDispatcher eventDispatcher;
 
+    private WorkerPool<CityGML> workerPool;
     private volatile boolean shouldRun = true;
 
     CityJSONReader(CityGMLInputFilter typeFilter, CounterFilter counterFilter, CityJSONInputFactory factory) {
@@ -46,40 +42,29 @@ public class CityJSONReader implements FeatureReader, EventHandler {
 
     @Override
     public void read(InputFile inputFile, WorkerPool<CityGML> workerPool) throws FeatureReadException {
-        try (org.citygml4j.builder.cityjson.json.io.reader.CityJSONReader reader = factory.createFilteredCityJSONReader(
-                factory.createCityJSONReader(inputFile.openStream()), typeFilter)) {
-            // read input file into a city model
-            CityModel cityModel = reader.read();
+        this.workerPool = workerPool;
 
-            // process city model members
-            process(cityModel.getCityObjectMember().iterator(), workerPool);
-            process(cityModel.getFeatureMember().iterator(), workerPool);
-            process(cityModel.getAppearanceMember().iterator(), workerPool);
-        } catch (CityJSONReadException | IOException e) {
+        try (CityJSONChunkReader reader = factory.createFilteredCityJSONReader(
+                factory.createCityJSONChunkReader(inputFile.openStream()), typeFilter)) {
+            reader.read(this::process);
+        } catch (Exception e) {
             throw new FeatureReadException("Failed to read CityJSON input file.", e);
         }
     }
 
-    private void process(Iterator<? extends FeatureProperty<?>> iter, WorkerPool<CityGML> workerPool) {
-        while (shouldRun && iter.hasNext()) {
-            AbstractFeature feature = iter.next().getFeature();
-
-            // unset parent to mark the feature as top-level
-            feature.unsetParent();
-
-            // remove feature from feature collection
-            iter.remove();
-
+    private void process(AbstractFeature feature) {
+        if (shouldRun) {
             if (feature instanceof CityGML) {
                 if (counterFilter != null && !(feature instanceof Appearance)) {
                     if (!counterFilter.isStartIndexSatisfied()) {
                         counterFilter.incrementStartIndex();
-                        continue;
+                        return;
                     }
 
                     counterFilter.incrementCount();
-                    if (!counterFilter.isCountSatisfied())
-                        continue;
+                    if (!counterFilter.isCountSatisfied()) {
+                        return;
+                    }
                 }
 
                 workerPool.addWork((CityGML) feature);
