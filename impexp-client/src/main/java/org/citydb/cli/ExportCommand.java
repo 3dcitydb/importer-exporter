75bb1eff34e9fd535772ffdfe2967e5d2569e64d
Claus Nagel <cnagel@virtualcitysystems.de>
2021-03-30 18:17:26 +0200

    added --compressed-format option the export command to specify the output format in compressed exports

--- impexp-client/src/main/java/org/citydb/cli/ExportCommand.java
+++ impexp-client/src/main/java/org/citydb/cli/ExportCommand.java
@@ -34,6 +34,7 @@ import org.citydb.cli.options.exporter.QueryOption;
 import org.citydb.config.Config;
 import org.citydb.config.project.database.DatabaseConnection;
 import org.citydb.config.project.exporter.ExportConfig;
+import org.citydb.config.project.exporter.OutputFormat;
 import org.citydb.database.DatabaseController;
 import org.citydb.log.Logger;
 import org.citydb.plugin.CliCommand;
@@ -50,6 +51,8 @@ import java.nio.file.Path;
         versionProvider = ImpExpCli.class
 )
 public class ExportCommand extends CliCommand {
+    enum CompressedFormat {CityGML, CityJSON}
+
     @CommandLine.Option(names = {"-o", "--output"}, required = true,
             description = "Name of the output file.")
     private Path file;
@@ -57,6 +60,10 @@ public class ExportCommand extends CliCommand {
     @CommandLine.Option(names = "--output-encoding", defaultValue = "UTF-8",
             description = "Encoding used for the output file (default: ${DEFAULT-VALUE}).")
     private String encoding;
+
+    @CommandLine.Option(names = "--compressed-format", paramLabel = "<format>",
+            description = "Output format to use for compressed exports: ${COMPLETION-CANDIDATES}.")
+    private CompressedFormat compressedFormat;
 
     @CommandLine.ArgGroup
     private ThreadPoolOption threadPoolOption;
@@ -109,6 +116,12 @@ public class ExportCommand extends CliCommand {
 
     private void setExportOptions(ExportConfig exportConfig) {
         exportConfig.getGeneralOptions().setFileEncoding(encoding);
+
+        if (compressedFormat != null) {
+            exportConfig.getGeneralOptions().setCompressedOutputFormat(compressedFormat == CompressedFormat.CityJSON ?
+                    OutputFormat.CITYJSON :
+                    OutputFormat.CITYGML);
+        }
 
         if (queryOption != null) {
             exportConfig.getAppearances().setExportAppearances(queryOption.isExportAppearances());
