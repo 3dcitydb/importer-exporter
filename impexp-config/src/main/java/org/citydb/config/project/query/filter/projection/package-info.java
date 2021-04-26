8a742b494e5cc24e56208bea03a155f0b4af5669
Claus Nagel <cnagel@virtualcitysystems.de>
2021-04-26 19:21:00 +0200

    updated source file headers

--- impexp-config/src/main/java/org/citydb/config/project/query/filter/projection/package-info.java
+++ impexp-config/src/main/java/org/citydb/config/project/query/filter/projection/package-info.java
@@ -1,16 +1,16 @@
 /*
  * 3D City Database - The Open Source CityGML Database
- * http://www.3dcitydb.org/
+ * https://www.3dcitydb.org/
  *
- * Copyright 2013 - 2019
+ * Copyright 2013 - 2021
  * Chair of Geoinformatics
  * Technical University of Munich, Germany
- * https://www.gis.bgu.tum.de/
+ * https://www.lrg.tum.de/gis/
  *
  * The 3D City Database is jointly developed with the following
  * cooperation partners:
  *
- * virtualcitySYSTEMS GmbH, Berlin <http://www.virtualcitysystems.de/>
+ * Virtual City Systems, Berlin <https://vc.systems/>
  * M.O.S.S. Computer Grafik Systeme GmbH, Taufkirchen <http://www.moss.de/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
