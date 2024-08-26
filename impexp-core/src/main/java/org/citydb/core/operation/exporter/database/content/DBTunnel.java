941ca20e42c02b4bfac8d09e0234259f8d8728dc
Claus Nagel <cnagel@virtualcitysystems.de>
2024-08-26 19:30:18 +0200

    fixed tunnel boundary surface export to include ADE properties

--- impexp-core/src/main/java/org/citydb/core/operation/exporter/database/content/DBTunnel.java
+++ impexp-core/src/main/java/org/citydb/core/operation/exporter/database/content/DBTunnel.java
@@ -147,7 +147,7 @@ public class DBTunnel extends AbstractFeatureExporter<AbstractTunnel> {
 						.addJoin(JoinFactory.left(cityObject, "id", ComparisonName.EQUAL_TO, opening.getColumn("id")));
 				openingADEHookTables = addJoinsToADEHookTables(TableEnum.TUNNEL_OPENING, opening);
 			}
-			surfaceADEHookTables = addJoinsToADEHookTables(TableEnum.THEMATIC_SURFACE, thematicSurface);
+			surfaceADEHookTables = addJoinsToADEHookTables(TableEnum.TUNNEL_THEMATIC_SURFACE, thematicSurface);
 		}
 		if (lodFilter.containsLodGreaterThanOrEuqalTo(2)
 				&& (projectionFilter.containsProperty("outerTunnelInstallation", tunnelModule)
